599fcdd146c0277546c67c1364bc577ef1057ad0
Farah CHAATOUF <farah.chaatouf@centralelille.fr>
2026-09-22 13:34:17 +0200

    soft skills

--- Portfolio_Farah_Chaatouf/src/Composants/Compétences.tsx
+++ Portfolio_Farah_Chaatouf/src/Composants/Compétences.tsx
@@ -1,12 +1,11 @@
 import Fleche from "./Fleche";
 import Menu2 from "./Menu2";
 
-
 export default function Competences() {
   return (
     <>
       <div className="w-full min-h-screen flex flex-col items-center">
-      <div className="flex">
+        <div className="flex">
           <div className="w-fit py-8 flex text-3xl justify-center items-center text-center font-semibold font-serif bg-linear-to-r from-blue-800 to-fuchsia-400 bg-clip-text text-transparent box-decoration-clone">
             Compétences
           </div>
@@ -15,18 +14,18 @@ export default function Competences() {
         <div className="w-full flex justify-center items-center">
           <Menu2 />
         </div>
-        <div className="flex w-11/12 items-start justify-center space-x-40 mt-20">
+        <div className="flex w-11/12 items-start justify-center space-x-10 mt-20">
           <div className="w-1/4 rounded-xl shadow-2xl p-5 flex flex-col justify-center items-center">
             <div className="w-fit mb-5 flex text-xl justify-center items-center text-center font-semibold font-serif bg-linear-to-r from-blue-800 to-fuchsia-400 bg-clip-text text-transparent box-decoration-clone">
               Langues
             </div>
-            <div className="w-full">
-              <div>Français (Maternel)</div>
-              <div>Anglais (A2)</div>
-              <div>Espagnol (A2)</div>
+            <div className="w-full px-5">
+              <li>Français (Maternel)</li>
+              <li>Anglais (A2)</li>
+              <li>Espagnol (A2)</li>
             </div>
           </div>
-          <div className="w-1/2 rounded-xl shadow-2xl p-5 flex flex-col justify-center items-center">
+          <div className="w-2/3 rounded-xl shadow-2xl p-5 flex flex-col justify-center items-center">
             <div className="w-fit mb-5 flex text-xl justify-center items-center text-center font-semibold font-serif bg-linear-to-r from-blue-800 to-fuchsia-400 bg-clip-text text-transparent box-decoration-clone">
               Codage
             </div>
@@ -69,10 +68,14 @@ export default function Competences() {
             <div className="w-fit mb-5 flex text-xl justify-center items-center text-center font-semibold font-serif bg-linear-to-r from-blue-800 to-fuchsia-400 bg-clip-text text-transparent box-decoration-clone">
               Soft Skills
             </div>
-            <div className="w-full">
-              <div>Français (Maternel)</div>
-              <div>Anglais (A2)</div>
-              <div>Espagnol (A2)</div>
+            <div className="w-full px-5">
+              <li>À l'écoute</li>
+              <li>Esprit d'équipe</li>
+              <li>Pédagogue</li>
+              <li>Autonomie</li>
+              <li>Pédagogue</li>
+              <li>Sens des responsabilités</li>
+              <li>Bonne capacité d'adapation</li>
             </div>
           </div>
         </div>
